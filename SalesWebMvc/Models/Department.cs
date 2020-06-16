c8e08bf419c0504644c938a0cafee4992737cb75
Renato <otonatoh@gmail.com>
2020-06-16 15:05:16 -0300

    criando o foreinkey

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -10,7 +10,6 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
-
         public Department()
         {
 
